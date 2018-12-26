beffe65f4d7c0795d24287ae2c0056831c8abea9
Dan Stroot <dan.stroot@gmail.com>
2018-12-25 21:12:52 -0800

    update dependencies

--- src/components/ImageItem/index.js
+++ src/components/ImageItem/index.js
@@ -1,3 +1,5 @@
+// https://mor10.com/power-of-wordpress-image-metadata/
+
 import React, { useState } from "react";
 import arrow4 from "./media/download-arrow.svg";
 
@@ -97,55 +99,3 @@ const ImageModal = ({ image, visible, close }) => {
 };
 
 export default ImageItem;
-
-// <div className="backdrop" style={{ backdropStyle }}>
-//   <div className="row">
-//     <div className="col">
-//       <button
-//         type="button"
-//         className="close"
-//         aria-label="Close"
-//         onClick={close}
-//       >
-//         <span aria-hidden="true">&times;</span>
-//       </button>
-//       <img className="img-fluid" src={image.full} alt={image.alt} />
-//       <br />
-//       <h1 className="display-6 d-inline">
-//         <a
-//           className=""
-//           href={image.full}
-//           aria-label="Download Google"
-//           download
-//         >
-//           <img className="mr-2" src={arrow4} width="30px" alt="arrow" />
-//         </a>
-//         {image.title}
-//       </h1>
-//       <p className="small text-muted">2800x1868 pixels - 0.2 Mb</p>
-//     </div>
-//   </div>
-// </div>
-
-// <div className="col-sm-4 mt-4">
-//   <img
-//     className="img-fluid"
-//     src={image.full}
-//     width="400px"
-//     height="400px"
-//     alt={image.alt}
-//     onClick={() => setVisible(true)}
-//   />
-//   <br />
-//   <p className="lead d-inline">
-//     <a
-//       className=""
-//       href={image.full}
-//       aria-label="Download Google"
-//       download
-//     >
-//       <img className="mr-2" src={arrow4} width="22px" alt="arrow" />
-//     </a>
-//     {image.title}
-//   </p>
-// </div>
